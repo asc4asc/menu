2b228598b6f4472f097319ba000efed4eeb14f15
Andreas <asc4asc@users.noreply.github.com>
2026-02-25 14:44:19 +0100

    Comment out kill command in safe_delete_ns
    Commented out the kill command in safe_delete_ns function.

--- net-path-iperf-test.sh
+++ net-path-iperf-test.sh
@@ -83,7 +83,7 @@ force_down_root() {
 safe_delete_ns() {
   local ns="$1"
   if ns_exists "$ns"; then
-    ip netns pids "$ns" | xargs -r kill -9 || true
+    # ip netns pids "$ns" | xargs -r kill -9 || true
     ip netns delete "$ns" || true
   fi
 }
